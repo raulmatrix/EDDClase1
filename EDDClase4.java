f5a6604ef40e112b2fcbf1a6915bb3ad030c259d
raulmatrix <101440848+raulmatrix@users.noreply.github.com>
2022-03-11 23:52:24 -0400

    Create EDDClase4.java

---
+++ EDDClase4.java
@@ -0,0 +1,23 @@
+
+package eddclase4;
+
+public class EDDClase4 {
+
+   
+    public static void main(String[] args) {
+        //Cadenas
+        
+        String nombre="Alicia";
+        String apellido;
+        String abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        apellido = "Herbas";
+        
+        System.out.println("El abecedario tiene: "+abecedario.length()+" caracteres");
+        
+        System.out.println("El nombre es: "+nombre.toUpperCase());
+        
+        
+        
+    }
+    
+}
