eddd630bb2aa703af0b4d211a0fa5449638b7c12
raulmatrix <101440848+raulmatrix@users.noreply.github.com>
2022-04-01 07:55:46 -0400

    Create EDDClase6.java

---
+++ EDDClase6.java
@@ -0,0 +1,64 @@
+
+package pdmclase6;
+
+public class PDMClase6 {
+
+    public static void main(String[] args) {
+        
+        String mes="enero";
+        /*
+        switch(mes){
+            case "enero":
+                System.out.println("Enero encontrado");
+                break;
+            case "febrero":
+                System.out.println("Febrero encontrado");
+                break;
+            case "marzo":
+                System.out.println("Marzo encontrado");
+                break;
+            default:
+                System.out.println("El mes no fue encontrado");
+                
+        }*/
+        
+        //estructura while
+        int i = 6;
+        while (i <= 5) {
+          System.out.println(i);
+          i++;
+        }
+        
+        //do while
+        int j=8;
+        do{
+            System.out.println(j);
+            j--;
+        }while(j > 5);
+        
+        /*int j=2;//inicio
+        int limite=30;//fin
+        while(j<limite){
+            System.out.println(j);
+            j=j+2;
+        }*/
+        
+        //for
+        
+        /*for(int i=0;i<10;i++){
+            System.out.print(i+"-");
+        }*/
+        
+        /*for(int i=2;i<30;i++){
+            i++;
+            System.out.print(i+"-");
+        }*/
+        
+        /*for(){
+            for(){
+            }
+        }*/
+        
+    }
+    
+}
