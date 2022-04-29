4e00441b35a4729b239546dfcd05d4a498f77bb6
raulmatrix <101440848+raulmatrix@users.noreply.github.com>
2022-04-28 23:27:50 -0400

    Add files via upload

---
+++ EDDClase7.java
@@ -0,0 +1,50 @@
+
+package eddclase7;
+
+public class EDDClase7 {
+
+   
+    public static void main(String[] args) {
+        //for 
+        /*int num=25;
+        for(int i=0;i<=num;i++){
+            if(i<num){
+                System.out.print(i+"-");
+            }else{
+                System.out.print(i);
+            } 
+        }*/
+        
+        //matriz con for anidados
+        /*int filas=5;
+        int col=6;
+        
+        for(int i=1;i<=filas;i++){//filas
+            for(int j=1;j<=col;j++){//columnas
+                if(j<col){
+                System.out.print((i*j)+"-");
+                }
+                else{
+                System.out.print((i*j));
+                }   
+            }
+            System.out.println("");
+        }*/
+        
+        //while
+        
+        /*int x=0;
+        
+        while(x<10){
+            System.out.print(x);
+            x++;
+        }*/
+        int y=1;
+        do{
+            System.out.print(y+"-");
+            y++;
+        }while(y<1);
+        
+    }
+    
+}
